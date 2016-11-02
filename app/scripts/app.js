73d93e8e937710e8adc34c3ec36f6ad1746ebbc8
Maeve Tierney <maevet27@gmail.com>
2016-11-02 15:12:02 -0400

    Initial Commit with skeleton created for app and enviornment set up for local server to run

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name blogApp
+ * @description
+ * # blogApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('blogApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
